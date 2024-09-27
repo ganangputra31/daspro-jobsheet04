2ae3c642aca92f9aa41631c9b0ac086fc7a1eca2
ganang andika kurnia putra <myinwork14@gmail.com>
2024-09-27 20:29:25 +0700

    question 2.3

---
+++ Kafe08.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Kafe08 {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String menu;
+        char ukuranCup;
+        int jumlah;
+        boolean keangotaan;
+
+        System.out.print("masukkan menu : ");
+        menu = sc.nextLine();
+        System.out.print("masukkan ukuran cup : ");
+        ukuranCup = sc.next().charAt(0);
+        System.out.print("masukkan jumlah : ");
+        jumlah = sc.nextInt();
+        System.out.print("masukkan keanggotaan (true/false): ");
+        keangotaan = sc.nextBoolean();
+
+        double hargaMenu = 0;
+
+        switch (menu.toLowerCase()) {
+            case "kopi":
+                hargaMenu = 12000;
+            break;
+            case "teh":
+                hargaMenu = 7000;
+            break;
+            case "coklat":
+                hargaMenu = 20000;
+            break;    
+        }
+        double totalHarga = hargaMenu * jumlah;
+
+        switch (ukuranCup) {
+            case 'S':
+                break;
+            case 'M':
+                totalHarga += 0.25 * totalHarga;
+                break;
+            case 'L':
+                totalHarga += 0.4 * totalHarga;
+                break;
+                default:
+                System.out.println("size is not available.");
+        }
+
+        double diskon = keangotaan ? 0.1 :0;
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+
+        System.out.println("Item pembelian: " + jumlah +" " + menu + "dengan ukuran cup " + ukuranCup);
+        System.out.println("Nonimal bayar:" + nominalBayar);
+
+    }
+}
+
+    
+
